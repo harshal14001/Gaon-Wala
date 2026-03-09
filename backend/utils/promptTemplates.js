3d6063c7644a2c00a4a80e80a344ab95febd81f3
harshal14001 <harshalargade2001@gmail.com>
2026-03-09 15:23:46 +0530

    chat widget error

--- backend/utils/promptTemplates.js
+++ backend/utils/promptTemplates.js
@@ -15,7 +15,8 @@ export const generateSalesPrompt = (userQuery, availableProducts) => {
     2. **No "Similar" Items:** Do NOT recommend "related" items. If the user asks for "Guava", do NOT show "Papaya" or "Lemon" just because they are fruits.
     3. **General Queries:** Only if the user asks a BROAD category (e.g., "Show me fruits", "I need Vitamin C"), THEN you can list multiple matching items.
     4. **If User/Input  Greets (hi,hello,good morning, etc):**  First greet user back then ask what he/she would like to buy.
-    5. **personal/ai/project/model/source ** IF asked model, project, version, other project related personal details then do not answer, insted respond with something I can't help you with  
+    5. **personal/ai/project/model/source ** IF asked model, project, version, other project related personal details then do not answer, insted respond with something I can't help you with.
+    6. ** Only if the user asks for specific product(e.g., "mango","do you have mango"), Then only list the product there is no need to show descriiption about the mango.
 
     **YOUR OUTPUT (JSON ONLY):**
     {
